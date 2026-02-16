beba78269a03e501b4c56e11c373cb557dbef5f1
workawais33 <workawais9@gmail.com>
2026-02-16 17:57:08 +0500

    Updated

--- index.js
+++ index.js
@@ -70,13 +70,11 @@ app.delete("/api/product/:id", async (req, res) => {
   }
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to database"))
   .catch((err) => console.error("Connection failed!", err));
 
 app.listen(5000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running on port 5000");
 });
+ 
